7e45ba6dcdc29454c548082cf152f5a710ff1017
Apoorva <apoorva07shukla@gmail.com>
2026-08-30 16:37:37 +0530

    error in character_arrays_practice.c fixed

--- 1/9.character_arrays_practice.c
+++ 1/9.character_arrays_practice.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
+int get_line(char line[], int lim);
+void copy(char from[], char to[]);
 
 int main(void) {
     int len, max; // length of current line, maximum length seen so far
@@ -18,16 +20,18 @@ int main(void) {
     return 0;
 }
 
-int get_line(char give_line[], int limit) {
+int get_line(char given_line[], int limit) {
     int i, c;
+    c = 0;
+
     for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        give_line[i] = c;
+        given_line[i] = c;
 
     if (c == '\n')
-        give_line[i] = c, ++i;
-    give_line[i] = '\0'; // null character
+        given_line[i] = c, ++i;
+    given_line[i] = '\0'; // null character
 
-    // (i < limit - 1), why not (i <= limit - 1) in the for loop, after all, the length of the array is 1000 (0 - 999). It is becayse - We need to restrict input leaving one space at the end of the array so that we can add a null character '/0' which is standard ending of an array. If input is taken to the last index of the array, we can no more add the null character '/0'; what happens then, I must find out.
+    // (i < limit - 1), why not (i <= limit - 1) in the for loop, after all, the length of the array is 1000 (0 - 999). It is becayse - We need to restrict input leaving one space at the end of the array so that we can add a null character '\0' which is standard ending of an array. If input is taken to the last index of the array, we can no more add the null character '\0'; what happens then, I must find out.
 
     return i;
 }
@@ -35,7 +39,7 @@ int get_line(char give_line[], int limit) {
 void copy(char from[], char to[]) {
     int i = 0;
 
-    // remember that assignment returns the value of from[i], meaning the variable assigning to one getting assigned. This is critical, because otherwise had the value of to[i] been returned, the while loop would have ended as and when the smaller array ends, which is undesirable, for we need the bigger array to decide when to end the loop, so that no input character is missed. So, that assignment will return a null character '/0' at the end of the from[i] array; that's how we know when the array ends, by '/0'.
-    while ((to[i] = from[i]) != '/0')
+    // remember that assignment returns the value of from[i], meaning the variable assigning to one getting assigned. This is critical, because otherwise had the value of to[i] been returned, the while loop would have ended as and when the smaller array ends, which is undesirable, for we need the bigger array to decide when to end the loop, so that no input character is missed. So, that assignment will return a null character '\0' at the end of the from[i] array; that's how we know when the array ends, by '\0'.
+    while ((to[i] = from[i]) != '\0')
         ++i;
 }
